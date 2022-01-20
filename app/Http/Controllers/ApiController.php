a2124820470a4bf04f0f80a3821560536c235600
Raka Janitra <rakajanitraa@hotmail.com>
2022-01-20 07:18:35 +0700

    api controller

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use JWTAuth;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+
+class ApiController extends Controller
+{
+    public function register(Request $request){
+        $data = $request->only('name', 'email', 'password');
+
+        $validator = Validator::make($data, [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|max:50'
+        ]);
+
+        // send failed response if request is not valid
+        if($validator->fails()){
+            return response()->json(['error' => $validator->messages()], 200);
+        }
+
+        $user = User::create([
+        	'name' => $request->name,
+        	'email' => $request->email,
+        	'password' => bcrypt($request->password)
+        ]);
+
+        // user created, return success response
+        return response()->json([
+            'success' => true,
+            'message' => 'User created successfully',
+            'data' => $user
+        ], Response::HTTP_OK);
+    }
+
+
+}
